2f0bb49c4512d4c26c60f1fc0d8c77586e38913e
Shmily701 <juanjuandie@tencent.com>
2022-06-14 10:50:13 +0800

    [type]title
    body, optional fields
    
    [TAPD]

---
+++ baseline.sh
@@ -0,0 +1,18 @@
+
+git filter-branch -f --env-filter '
+
+OLD_EMAIL="juanjuandie@tencent.com"
+CORRECT_NAME="Shmily701"
+CORRECT_EMAIL="shmilytzero@163.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
